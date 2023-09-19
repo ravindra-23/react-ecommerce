6e4ef5acb061d8ea7dbcbf09235f8d05449a3218
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-19 16:26:41 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Home from './scenes/home/Home'
+import ItemDetails from './scenes/itemDetails/ItemDetails'
+import Navbar from './scenes/global/Navbar'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  },[pathname])
+
+  return null;
+}
+
 function App() {
   return (
-    <div className="App">
-      React app working
+    <div className="app">
+      <Router>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/item:itemId" element={ <ItemDetails /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
