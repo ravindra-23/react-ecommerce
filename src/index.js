d543146eebc1278455600f6a1d5f449f522b2741
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-19 16:52:54 +0530

    added store provider

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './theme'
+import store from './state/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
