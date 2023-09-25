4343eca96b6d7cda680b207800456c31944cdcb2
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-25 22:18:23 +0530

    added cartslice

--- src/state/cartSlice.js
+++ src/state/cartSlice.js
@@ -13,12 +13,40 @@ export const cartSlice = createSlice({
         setItems: (state, action) => {
             state.items = action.payload;
         },
+
         setIsCartOpen: (state) => {
             state.isCartOpen = !state.isCartOpen;
         },
+
+        addToCart: (state, action) => {
+            state.cart = [...state.cart, action.payload.item];
+        },
+
+        removeFromCart: (state, action) => {
+            state.cart = state.cart.filter((item) => item.id !== action.payload.id)
+        },
+
+        increaseQuantity: (state, action) => {
+            state.cart = state.cart.map((item) => {
+                if(item.id === action.payload.id) {
+                    item.count++
+                }
+                return item;
+            })
+        },
+
+        decreaseQuantity: (state, action) => {
+            state.cart = state.cart.map((item) => {
+                if(item.id === action.payload.id && item.count > 1) {
+                    item.count--
+                }
+                return item;
+            })
+        },
+
     }
 });
 
-export const { setItems, setIsCartOpen } = cartSlice.actions;
+export const { setItems, setIsCartOpen, addToCart, removeFromCart, increaseQuantity, decreaseQuantity } = cartSlice.actions;
 
 export default cartSlice.reducer
